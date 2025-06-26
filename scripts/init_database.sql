58e423fe612001f64b5e4292e9d004189128be02
Tang Yu Xuan <67047462+doodledaron@users.noreply.github.com>
2025-06-26 13:05:41 +0800

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,34 @@
+-- Database: DataWarehouse
+/*
+============================================================
+Create Database and Schemas
+============================================================
+
+Script Purpose:
+    This script creates a new database named 'DataWarehouse' after checking if it already exists.
+    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas
+    within the database: 'bronze', 'silver', and 'gold'.
+
+WARNING:
+    Running this script will drop the entire 'DataWarehouse' database if it exists.
+    All data in the database will be permanently deleted. Proceed with caution
+    and ensure you have proper backups before running this script.
+*/
+
+DROP DATABASE IF EXISTS "DataWarehouse";
+
+CREATE DATABASE "DataWarehouse"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'C'
+    LC_CTYPE = 'C'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
+
+CREATE SCHEMA bronze;
+CREATE SCHEMA silver;
+CREATE SCHEMA gold;
